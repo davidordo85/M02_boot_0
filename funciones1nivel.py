7a9f4453d001523812b3e1d8db67c8fd332a996d
davidordo85 <63961231+davidordo85@users.noreply.github.com>
2020-04-21 18:55:57 +0200

    lambdas

--- funciones1nivel.py
+++ funciones1nivel.py
@@ -4,6 +4,9 @@ def normal(i):
 def cuadrado(x):
     return x * x
 
+def cubo(x):
+    return x**3
+
 
 def sumaTodos(limitTo, f):
     resultado = 0
@@ -12,5 +15,6 @@ def sumaTodos(limitTo, f):
         
     return resultado
 
-print(sumaTodos(100, normal))
-print(sumaTodos(3, cuadrado))
+if __name__ == '__main__':    
+    print(sumaTodos(100, normal))
+    print(sumaTodos(3, cuadrado))
