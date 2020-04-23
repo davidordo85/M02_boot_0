a9800c0ed46c3e9af7d308bf1c3a1dbd3c6b15bd
davidordo85 <63961231+davidordo85@users.noreply.github.com>
2020-04-23 13:18:33 +0200

    Primera versión de perro

---
+++ Objetos.py
@@ -0,0 +1,17 @@
+import turtle
+
+tortu = turtle.Turtle()
+tortu01 = turtle.Turtle()
+lentorra= turtle.Turtle()
+
+lentorra.speed(1)
+tortu.shape('turtle')
+tortu.color('blue')
+tortu.fd(50)
+tortu.speed(5)
+
+
+tortu01.color('green')
+tortu01.left(90)
+tortu01.fd(50)
+tortu01.speed(5)
